6b436bc19daf6b710f84a0825e8a111a4d28e5d6
Robert Quitt <robertquitt@berkeley.edu>
2017-10-26 18:29:31 -0700

    Fix install.sh syntax

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo Installing dotfiles in $DIR \n
 
-if [ ! -e ~/.vim/autoload/plug.vim ] then
+if [ ! -e ~/.vim/autoload/plug.vim ]; then
   echo Installing vim-plug...
   curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
