7716cf0cb3d4c23da396154ce3f769858b69a57e
Robert Quitt <robertquitt@gmail.com>
2018-03-15 15:39:00 -0700

    Check in .ssh/config

--- diff.sh
+++ diff.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo Diffing dotfiles
 
-for FILE in .vimrc .tmux.conf .gitconfig .ssh/authorized_keys
+for FILE in .vimrc .tmux.conf .gitconfig .ssh/authorized_keys .ssh/config
 do
   diff -q $DIR/$FILE ~/$FILE || vimdiff $DIR/$FILE ~/$FILE
 done
