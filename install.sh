a302f423cefce6c4b9ad3c6163d13ece12b60368
Robert Quitt <robertquitt@gmail.com>
2018-08-12 23:06:35 -0700

    Experimental: use links for Windows

--- install.sh
+++ install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -e
+
 echo Installing dotfiles from $DIR
 
 if [ ! -e ~/.vim/autoload/plug.vim ]; then
   echo Installing vim-plug...
-  curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  curl -s -Lo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-for FILE in .vimrc .tmux.conf .gitconfig .ssh/authorized_keys
+for FILE in .vimrc .tmux.conf .gitconfig .ssh/authorized_keys .ssh/config
 do
-  diff -q $DIR/$FILE ~/$FILE || cp -i -v $DIR/$FILE ~/$FILE
+  if [[ $(uname -s) = *"NT"* ]]; then
+    diff -q $DIR/$FILE ~/$FILE || ln -iv $DIR/$FILE ~/$FILE
+  else
+    diff -q $DIR/$FILE ~/$FILE || ln -ivs $DIR/$FILE ~/$FILE
+  fi
 done
 
 echo Installing Vim plugins
 
 vim -E -c "PlugInstall" -c "q" -c "q"
 
+# vim: sw=2 et ts=2
--- pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo Copying dotfiles back into $DIR
-
-cp -v ~/.vimrc $DIR/.vimrc
-cp -v ~/.tmux.conf $DIR/.tmux.conf
-
